14abb1c158c4256d505cb4369f4b89a952ffeb56
chaudbrush <chaudbrush@gmail.com>
2025-03-13 18:30:21 +0000

    norminette corrections

--- ft_printf.h
+++ ft_printf.h
@@ -6,20 +6,20 @@
 /*   By: vloureir <vloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:46:35 by vloureir          #+#    #+#             */
-/*   Updated: 2025/03/13 15:55:28 by vloureir         ###   ########.fr       */
+/*   Updated: 2025/03/13 18:29:43 by vloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FTPRINTF_H
+#ifndef FT_PRINTF_H
 
-# define FTPRINTF_H
+#define FT_PRINTF_H
 
 # include <stdarg.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-int ft_printf(const char *format, ...);
+int	ft_printf(const char *format, ...);
 int	mod_putchar(char c);
 int	mod_putnbr(int nb);
 int	mod_putnbr_u(unsigned int nb);
